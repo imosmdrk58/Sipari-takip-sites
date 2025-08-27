ba3d6b6f6cc593a34e817308bc0efaa3f8ae3e46
Lucerna <83871124+imosmdrk58@users.noreply.github.com>
2025-08-27 21:58:05 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -25,4 +26,5 @@ app.get('/orders', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
